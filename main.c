3e929db4f3031968eb921866f8ebdc9acc6cbb98
code-sharad <codesharad@gmail.com>
2023-08-18 18:45:45 +0530

    Html repo added

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+    int A = 34;
+    int B = 23;
+     A B;
+    return 0;
+}
---
+++ p.py
@@ -0,0 +1,12 @@
+age=45
+if age > 40:
+
+if age > 60:
+    print('Turning old')
+
+else:
+    print('Middle age')
+
+else:
+    
+    print('Young age')
